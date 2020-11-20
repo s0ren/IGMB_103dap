3bde0ae8b1ee02382f6e62a7c0ea3e7a5a1a665e
Emil <45782615+EMIL-O@users.noreply.github.com>
2020-11-20 09:50:56 +0100

    Update test and class

--- IGMB_103dap/Investeringskonto.cs
+++ IGMB_103dap/Investeringskonto.cs
@@ -6,15 +6,16 @@ namespace IGMB_103dap
 {
     public class Investeringskonto : Konto
     {
-        public double Rente { get; set; }
+
+        private double Rente { get; set; }
         public Investeringskonto(double startSaldo, double renteSats) : base(startSaldo)
         {
-            double rente = renteSats;
+            Rente = renteSats;
         }
 
-        public void beregnRente(double startSaldo, double renteSats)
+        public double beregnRente()
         {
-            double investeringsRente = startSaldo * renteSats;
+            return this.saldo * this.Rente;
         }
     }
 }
---
+++ XUnitTestProject1/InvesteringskontoTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IGMB_103dap;
+
+namespace XUnitTestProject1
+{
+    class InvesteringskontoTest
+    {
+        Investeringskonto Konto;
+        public InvesteringskontoTest()
+        {
+            Konto = new Investeringskonto(10000, 0.33);
+            Konto.beregnRente();
+        }
+    }
+}
