12ab42fd6cbdc435e7155d2808aacdbb73508104
LukasFolmer <45756566+LukasFolmer@users.noreply.github.com>
2020-11-20 10:59:06 +0100

    Update InvesteringsTest.cs

--- XUnitTestProject1/InvesteringsTest.cs
+++ XUnitTestProject1/InvesteringsTest.cs
@@ -7,9 +7,18 @@ namespace XUnitTestProject1
     public class InvesteringsTest
     {
         Investeringskonto konto;
+
+        
         InvesteringsTest()
         {
             konto = new Investeringskonto(10000, -0.03);
+        }
+
+    
+
+        [Fact]
+        public void beregn()
+        {
             konto.BeregnRente();
         }
 
