0253f825d78e4776d5538ed859dd0c7a2a1f080a
annadavidsen <anna.m.davidsen@gmail.com>
2020-11-20 09:19:49 +0100

    Renteopsparingskonto + test oprettet

---
+++ IGMB_103dap/Renteopsparingskonto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IGMB_103dap
+{
+    public class Renteopsparingskonto : Konto
+    {
+        //Rentesats pr. forentning
+        public double Rentesats { get; set; }
+        public Renteopsparingskonto(double startSaldo, double rentesats) 
+            : base(startSaldo)
+        {
+            Rentesats = rentesats;
+        }
+        /// <summary>
+        /// Beregner ny saldo efter en forrentning
+        /// </summary>
+        /// <returns>Ny saldo</returns>
+        public double Forrentning()
+        {
+            var nySaldo = saldo * (1 + Rentesats / 100);
+            return nySaldo;
+        }
+    }
+}
