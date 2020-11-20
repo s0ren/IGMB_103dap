3e8c4b0a517b8d3a94185336e0cb9396664e0202
Emil <45782615+EMIL-O@users.noreply.github.com>
2020-11-20 10:17:05 +0100

    update

---
+++ XUnitTestProject1/InvesteringsTest.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using IGMB_103dap;
+
+namespace XUnitTestProject1
+{
+    public class InvesteringsTest
+    {
+        Investeringskonto konto;
+        InvesteringsTest()
+        {
+            konto = new Investeringskonto(10000, -0.03);
+            konto.BeregnRente();
+        }
+
+    }
+}
