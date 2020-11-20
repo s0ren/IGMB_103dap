4cc5af73c7aaaa3793417dca5bcfc5242e7942a4
Søren Magnusson <smag@tec.dk>
2020-11-20 09:14:02 +0100

    Mere dokumentation i formaterede kommentarer

--- IGMB_103dap/Konto.cs
+++ IGMB_103dap/Konto.cs
@@ -17,13 +17,27 @@ namespace IGMB_103dap
             saldo = startSaldo;
         }
 
+        /// <summary>
+        /// En egenskab der indeholder den aktuelle saldo.
+        /// Dvs. havd der til enhvertid står på kontoen.
+        /// </summary>
         public double saldo { get; private set; }
 
+        /// <summary>
+        /// Indsætter et beløb og opdaterer saldo tilsvarende.
+        /// Bør kun kunne indsætte postive beløb, større end nul.
+        /// </summary>
+        /// <param name="beløb">Postivt tal, større end nul</param>
         public void Indsæt(double beløb)
         {
             this.saldo += beløb;
         }
 
+        /// <summary>
+        /// Hæver et et beløb, og opdaterer daldo
+        /// kan kun hæve positive beløb, større edn nul
+        /// </summary>
+        /// <param name="beløb">positivt tal større end 0</param>
         public void Hæv(double beløb)
         {
             if (saldo >= beløb)
